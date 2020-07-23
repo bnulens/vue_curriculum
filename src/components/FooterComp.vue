774118a61125f5874850367294c763aac8ffe96c
Brecht <brechtnulens@gmail.com>
2020-07-23 15:56:24 +0200

    Moved media links and icons to header navbar

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,16 +1,6 @@
 <template>
     <footer>
-        <div class="ref-icons">
-            <a href="https://github.com/bnulens" target="_blank">
-                <img class="ref-icon github" src="/assets/icons/github.svg" alt="GitHub logo">
-            </a>
-            <a href="https://www.linkedin.com/in/brecht-nulens/" target="_blank">
-                <img class="ref-icon" src="/assets/icons/linkedIn.svg" alt="LinkedIn logo">
-            </a>
-            <a href="mailto:brechtnulens@gmail.com" target="_blank">
-                <img class="ref-icon" src="/assets/icons/gmail.svg" alt="Gmail logo">
-            </a>
-        </div>
+        <span>&copy;Brecht Nulens</span>
     </footer>
 </template>
 
@@ -23,39 +13,7 @@
         justify-content: center;
         align-items: center;
         height: 100px;
-        width: 100vw;
-        background-color: rgb(70, 70, 70);
+        width: 100%;
+        border-top: 1px solid grey;
     }
-
-    .ref-icon {
-        width: 28px;
-        margin-left: 20px;
-        filter: grayscale(1);
-        transition: filter 0.3s;
-    }
-
-
-    @media screen and (min-width: 768px){
-    .ref-icon {
-        width: 28px;
-        margin-left: 20px;
-        filter: grayscale(0.7);
-        transition: filter 0.3s;
-    }
-
-    .ref-icon:hover {
-        filter: grayscale(0);
-    }
-
-    .github{
-        width: 28px;
-        margin-left: 20px;
-        filter: invert(0);
-        transition: ease filter 0.3s;
-    }
-
-    .github:hover {
-        filter: invert(1);
-    }
-}
 </style>
