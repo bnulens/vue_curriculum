aa659dbb15646b917f5f3cf4a8af1ef202b08fc8
Brecht <brechtnulens@gmail.com>
2020-07-23 15:55:35 +0200

    Adjusted header navbar

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,13 +1,26 @@
 <template>
     <header>
-        <ul class="navbar">
-            <li v-for="(item, i) in navItems" :key="i">
-                <a :href="`#${item}`">{{ item }}</a>
-            </li>
-            <li>
-                <a href="mailto:brechtnulens@gmail.com">contact</a>
-            </li>
-        </ul>
+        <div class="header-wrapper">
+            <div class="ref-icons">
+                <a href="https://github.com/bnulens" target="_blank" rel="norefferer noopener">
+                    <img class="ref-icon" src="/assets/icons/github.svg" alt="GitHub">
+                </a>
+                <a href="https://www.linkedin.com/in/brecht-nulens/" target="_blank" rel="norefferer noopener">
+                    <img class="ref-icon" src="/assets/icons/linkedin2.svg" alt="LinkedIn">
+                </a>
+                <a href="mailto:brechtnulens@gmail.com" target="_blank" rel="norefferer noopener">
+                    <img class="ref-icon" src="/assets/icons/email.svg" alt="Gmail">
+                </a>
+            </div>
+            <ul class="navbar">
+                <li v-for="(item, i) in navItems" :key="i">
+                    <a :href="`#${item}`">{{ item }}</a>
+                </li>
+                <li>
+                    <a href="mailto:brechtnulens@gmail.com" rel="norefferer noopener">contact</a>
+                </li>
+            </ul>
+        </div>
     </header>
 </template>
 
@@ -23,14 +36,16 @@ export default {
 </script>
 
 <style scoped>
-    header {
+    .header-wrapper {
         display: flex;
-        justify-content: center;
+        justify-content: space-between;
+        align-items: center;
+        height: 40px;
+        padding: 24px;
     }
 
     ul {
         display: flex;
-        padding-top: 24px;
     }
 
     li {
@@ -38,7 +53,30 @@ export default {
     }
 
     a {
+        font-size: 18px;
+        font-weight: 100;
         text-decoration: none;
-        color: white;
+        color: black;
+    }
+
+    .ref-icon {
+        width: 28px;
+        margin-left: 20px;
+        filter: grayscale(1);
+        transition: filter 0.3s;
+    }
+
+
+    @media screen and (min-width: 768px){
+        .ref-icon{
+            width: 28px;
+            margin-left: 20px;
+            filter: invert(0);
+            transition: ease filter 0.3s;
+        }
+
+        .ref-icon:hover {
+            filter: invert(1);
+        }
     }
 </style>
