48500ea633b65e760784a45ed90b6fd12280ee9d
Brecht <brechtnulens@gmail.com>
2020-07-25 03:45:25 +0200

    Added HeroComp with separate styling

--- src/components/BodyComp.vue
+++ src/components/BodyComp.vue
@@ -1,5 +1,6 @@
 <template>
     <main class="container">
+        <HeroComp/>
         <AboutComp/>
         <CoderComp/>
         <SkillComp/>
@@ -8,18 +9,26 @@
 </template>
     
 <script>
+import HeroComp from './sections/HeroComp'
 import AboutComp from './sections/AboutComp'
 import SkillComp from './sections/SkillComp'
 import ExperienceComp from './sections/ExperienceComp'
 import CoderComp from './sections/CoderComp'
 
 export default {
-    components: { AboutComp, SkillComp, ExperienceComp, CoderComp }
+    components: { HeroComp, AboutComp, SkillComp, ExperienceComp, CoderComp }
 }
 </script>
 <style scoped>
     .container {
         position: relative;
+    }
+
+    .hero {
+        max-width: none;
+        align-self: center;
+        width: auto;
+        margin-bottom: 56px;
     }
 
     @media screen and (min-width: 768px) {
