b4f2b2c21699e29aa066da767820f8d038b5db55
Brecht <brechtnulens@gmail.com>
2020-07-28 22:11:04 +0200

    Imported and registered SKillChart comp

--- src/components/sections/SkillComp.vue
+++ src/components/sections/SkillComp.vue
@@ -1,6 +1,7 @@
 <template>
     <section id="skills">
         <h2>Skills</h2>
+        <SkillChart/>
         <TitledIconList title="As a developer" :iconList="devSkillIcons"/>
         <TitledIconList title="As a designer" :iconList="designerSkillIcons"/>
     </section>
@@ -8,11 +9,12 @@
 
 <script>
 import TitledIconList from '../skills/TitledIconList'
+import SkillChart from '../skills/SkillChart'
 import devSkillIcons from '../../assets/devskillicons'
 import designerSkillIcons from '../../assets/designerskills'
 
 export default {
-    components: { TitledIconList },
+    components: { TitledIconList, SkillChart },
     data() {
         return {
             devSkillIcons,
